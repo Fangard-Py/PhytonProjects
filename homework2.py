88250cf173aca1795a4dd72f7ededfae4c011a20
Fangard-Py <pahan89reg@gmail.com>
2024-07-22 23:11:24 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+course = "Phyton"
+hours_spent = 1.5
+completed_hw = 12
+time_to_complete = (1.5 / 12)
+print\
+    (course, "всего задач:", completed_hw, ", затрачено часов:",
+     hours_spent, ", среднее время выполнения", time_to_complete, "часа.")
