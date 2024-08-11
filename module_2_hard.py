d5a1a82643694707360254f5ce74f34b64ae64a3
Fangard-Py <pahan89reg@gmail.com>
2024-08-11 20:25:18 +0500

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+import random
+
+first_stone = []
+second_stone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+rand_num = random.choice([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+first_stone.append(rand_num)
+print(first_stone)
+for i in first_stone:
+    if i == 0:
+        continue
+    for j in second_stone:
+        if i % j == 0:
+            print(j)
