d604083c539e3c0de99ea0c3682b5a20b228744b
Fangard-Py <pahan89reg@gmail.com>
2024-08-11 21:07:18 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,7 +2,7 @@ import random
 
 first_stone = []
 second_stone = range(1, 20)
-rand_num = random.choice([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+rand_num = random.choice(range(3, 20))
 first_stone.append(rand_num)
 print(first_stone)
 for i in first_stone:
