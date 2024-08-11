7cef09870612f340736e19167145810a4d799a26
Fangard-Py <pahan89reg@gmail.com>
2024-08-11 22:18:48 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,13 +1,12 @@
 import random
-
+result = str()
 first_stone = []
 second_stone = range(1, 20)
 rand_num = random.choice(range(3, 20))
 first_stone.append(rand_num)
 print(first_stone)
 for i in first_stone:
-    if i == 0:
-        continue
     for j in second_stone:
         if i % j == 0:
-            print(j)
+            result += f'{j}'
+            print(f'{first_stone} - {result}')
