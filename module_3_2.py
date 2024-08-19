64a389698f8dbbbc51a9768814455dd754c91ffb
Fangard-Py <pahan89reg@gmail.com>
2024-08-20 00:17:12 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -6,7 +6,11 @@ def send_email(message, recipient, sender='university.help@gmail.com'):
         print('Нельзя отправить письмо самому себе!')
     elif sender == 'university.help@gmail.com':
         print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
-    elif ("@" and (".com" or ".ru" or ".net")) not in recipient and ("@" and (".com" or ".ru" or ".net")) not in sender:
+    elif '@' not in recipient or '@' not in sender:
+        print('Невозможно отправить письмо с адреса ', sender, 'на адрес', recipient)
+    elif not recipient.endswith(('.ru', '.com', '.net')):
+        print('Невозможно отправить письмо с адреса ', sender, 'на адрес', recipient)
+    elif not sender.endswith(('.ru', '.com', '.net')):
         print('Невозможно отправить письмо с адреса ', sender, 'на адрес', recipient)
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса',sender, 'на адрес', recipient)
